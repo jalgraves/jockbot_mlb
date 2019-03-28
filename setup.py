fa2fd6b31e21afb90d4d148a4a4ad8f23ba21746
Jonny Graves <jgraves@dyn.com>
2019-03-28 18:21:58 -0400

    remove jockbot_mlb/async_mlb.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='jockbot_mlb',
-      version='0.0',
+      version='0.1',
       description='Retrieve info from MLB API',
       url='http://github.com/jalgraves/jockbot_mlb',
       author='Jonny Graves',
